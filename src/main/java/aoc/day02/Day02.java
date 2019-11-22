234b26149fc5c89cd389a9293160294e682b60b9
oy29yv <marco.de.bie@ing.com>
2019-11-22 23:36:41 +0100

    Used streams for day2 part 1

--- src/main/java/aoc/day02/Day02.java
+++ src/main/java/aoc/day02/Day02.java
@@ -6,46 +6,26 @@ import com.google.common.base.CharMatcher;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Day02 implements Day {
 
     static int countChars(String testString, char testChar) {
-        return CharMatcher.is(testChar).countIn(testString);   // how many testChars are there in testString!
+        return CharMatcher.is(testChar).countIn(testString);
     }
 
     @Override
     public String part1(List<String> input) {
-        char temp;
+        List<String> foundDoubles = input.stream().
+                filter(inputLine -> IntStream.range(0, inputLine.length())
+                        .anyMatch(i -> (countChars(inputLine, inputLine.charAt(i))) == 2))
+                .collect(Collectors.toList());
 
-        LinkedList<Character> foundDoubles = new LinkedList<>();     // linkedList to save the found doubles, this time I need to be able to add duplicates
-        LinkedList<Character> foundTriples = new LinkedList<>();     // linkedList to save the found triples, this time I need to be able to add duplicates
-
-        for (String inputLine : input) {
-            boolean foundDouble = false;
-            boolean foundTriple = false;
-            for (int i = 0; i < inputLine.length(); i++)                    // loop through the array
-            {
-                temp = inputLine.charAt(i);                                // the character that needs to be tested
-                switch (countChars(inputLine, temp)) {                     // how many times is this character in the string
-                    case 3: {
-                        if (!foundTriple) {
-                            foundTriples.add(temp);                         // add to list of triplets
-                            foundTriple = true;                             // remember that a triplet has been found, so I can stop the iteration later
-                        }
-                        break;                                              // no need to check for double, it is already a triplet
-                    }
-                    case 2: {
-                        if (!foundDouble) {
-                            foundDoubles.add(temp);                         // add to list of doubles
-                            foundDouble = true;                             // remember that a double has been found, so I can stop the iteration later
-                        }
-
-                    }
-                }
-                if (foundDouble && foundTriple)
-                    break;                     // if a double and a triplet has been found, then stop this iteration.
-            }
-        }
+        List<String> foundTriples = input.stream().
+                filter(inputLine -> IntStream.range(0, inputLine.length())
+                        .anyMatch(i -> (countChars(inputLine, inputLine.charAt(i))) == 3))
+                .collect(Collectors.toList());
 
         return input.isEmpty() ? "" : String.valueOf(foundDoubles.size() * foundTriples.size());
     }
