26003cd7f7cd178f438512c734078fc6fdf797c1
oy29yv <marco.de.bie@ing.com>
2019-11-24 21:14:48 +0100

    Kept day2 part 2 as it was

--- src/main/java/aoc/day02/Day02.java
+++ src/main/java/aoc/day02/Day02.java
@@ -2,6 +2,7 @@ package aoc.day02;
 
 import aoc.Day;
 import com.google.common.base.CharMatcher;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import static aoc.helper.ListHelper.convertStringToCharList;
 import static aoc.helper.StringHelper.countChars;
 
 public class Day02 implements Day {
@@ -38,16 +40,18 @@ public class Day02 implements Day {
             for (String inputSecondLoop : input) {                              // loop through the array a second time, now I have to elements of the array at the same time
                 LinkedList<Character> foundDifference = new LinkedList<>();    // initialize a list to count different characters
                 if (Objects.equals(inputFirstLoop, inputSecondLoop))
-                    continue;                                                   // Do not have to check the element against itself
+                    continue;
+
+                // Do not have to check the element against itself
                 for (int i = 0; i < inputFirstLoop.length(); i++) {             // Loop through both the elements to determine which characters are the same
                     if (inputFirstLoop.charAt(i) != inputSecondLoop.charAt(i)) {// if they are not the same add the character to the list
                         resultChar = inputFirstLoop.charAt(i);                  // save the Char that is not the same for later use
                         foundDifference.add(resultChar);
                     }
                 }
+
                 foundIt = (foundDifference.size() == 1);                        // finaly if there was only one element in the list it is found, set foundIt so I can terminate
-                if (foundIt)
-                    break;                                                      // terminate the iteration now and I the higher loop.....
+                if (foundIt) break;                                                      // terminate the iteration now and I the higher loop.....
             }
             // save the found value , subtract the different character in a variable
             foundString = (inputFirstLoop.replace(String.valueOf(resultChar), ""));
