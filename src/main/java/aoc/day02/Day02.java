8396edeada034180ae3d2068ed48fd509245edb1
oy29yv <marco.de.bie@ing.com>
2019-12-02 21:36:47 +0100

    Day2 part2 refactored

--- src/main/java/aoc/day02/Day02.java
+++ src/main/java/aoc/day02/Day02.java
@@ -8,18 +8,16 @@ public class Day02 implements Day {
 
     private String[] runProgram(String[] inputProgram){
         for (int i = 0; i < inputProgram.length; i += 4) {
-            int oppCode = Integer.parseInt(inputProgram[i]);
-            if (oppCode == 99) break;
-            int reg1 = Integer.parseInt(inputProgram[i + 1]);
-            int reg2 = Integer.parseInt(inputProgram[i + 2]);
-            int dest = Integer.parseInt(inputProgram[i + 3]);
+            if (Integer.parseInt(inputProgram[i]) == 99) break;
 
-            if (oppCode == 1) {
-                inputProgram[dest] = String.valueOf(Integer.parseInt(inputProgram[reg1]) +
-                        Integer.parseInt(inputProgram[reg2]));
+            if (Integer.parseInt(inputProgram[i]) == 1) {
+                inputProgram[Integer.parseInt(inputProgram[i + 3])] = String.valueOf(
+                        Integer.parseInt(inputProgram[Integer.parseInt(inputProgram[i + 1])]) +
+                                Integer.parseInt(inputProgram[Integer.parseInt(inputProgram[i + 2])]));
             } else {
-                inputProgram[dest] = String.valueOf(Integer.parseInt(inputProgram[reg1]) *
-                        Integer.parseInt(inputProgram[reg2]));
+                inputProgram[Integer.parseInt(inputProgram[i + 3])] = String.valueOf(
+                        Integer.parseInt(inputProgram[Integer.parseInt(inputProgram[i + 1])]) *
+                                Integer.parseInt(inputProgram[Integer.parseInt(inputProgram[i + 2])]));
             }
         }
         return inputProgram;
@@ -27,18 +25,18 @@ public class Day02 implements Day {
 
     @Override
     public String part1(List<String> input) {
-        return input.isEmpty() ? "" : runProgram(input.toArray(new String[input.size()]))[0];
+        return input.isEmpty() ? "" : runProgram(input.toArray(new String[0]))[0];
     }
 
     @Override
     public String part2(List<String> input) {
-        boolean found = false;
         int a;
         int b = 0;
+        boolean found = false;
 
         for (a = 0; a <= 99; a++) {
             for (b = 0; b <= 99; b++) {
-                String[] programArray = input.toArray(new String[input.size()]);
+                String[] programArray = input.toArray(new String[0]);
                 programArray[1] = String.valueOf(a);
                 programArray[2] = String.valueOf(b);
                 found = (Integer.parseInt(runProgram(programArray)[0]) == 19690720);
