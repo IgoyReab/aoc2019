1b8df99607d5dd765ec204e440d4b80f2d23fbd0
oy29yv <marco.de.bie@ing.com>
2019-12-07 19:33:52 +0100

    Day07 refactored

--- src/main/java/aoc/day07/Day07.java
+++ src/main/java/aoc/day07/Day07.java
@@ -6,9 +6,10 @@ import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Day07 implements Day {
-
 
     @Data
     class Amplifier {
@@ -136,169 +137,52 @@ public class Day07 implements Day {
             }
             return result;
         }
-
     }
 
-
-    private Integer runProgram(String[] inputProgram, int phaseSetting, int inputSetting){
-        int count = 0;
-        int oppCode = 0;
-        int parameter1;
-        int parameter2;
-        int parameter3;
-        int result = 0;
-        boolean positionMode1 = true;
-        boolean positionMode2 = true;
-
-        boolean phase = true;
-
-        while (oppCode != 99) {
-            switch (inputProgram[count].length()) {
-                case 1:
-                case 2:    {
-                    oppCode = Integer.parseInt(inputProgram[count]);
-                    positionMode2 = true;
-                    positionMode1 = true;
-                    break;
-                }
-                case 3 : {
-                    oppCode =  Integer.parseInt(String.valueOf(inputProgram[count].charAt(2)));
-                    positionMode2 = true;
-                    positionMode1 = (0 == Integer.parseInt(String.valueOf(inputProgram[count].charAt(0))));
-                    break;
-                }
-                case 4: {
-                    oppCode =  Integer.parseInt(String.valueOf(inputProgram[count].charAt(3)));
-                    positionMode2 = (0 == Integer.parseInt(String.valueOf(inputProgram[count].charAt(0))));
-                    positionMode1 = (0 == Integer.parseInt(String.valueOf(inputProgram[count].charAt(1))));
-                    break;
-                }
-                case 5: {
-                    oppCode =  Integer.parseInt(String.valueOf(inputProgram[count].charAt(4)));
-                    positionMode2 = (0 == Integer.parseInt(String.valueOf(inputProgram[count].charAt(1))));
-                    positionMode1 = (0 == Integer.parseInt(String.valueOf(inputProgram[count].charAt(2))));
-                    break;
-                }
-            }
-
-            if (oppCode == 9) oppCode = 99;
-
-            switch (oppCode){
-                case 1 :
-                case 2 : {
-                    parameter1 = Integer.parseInt(inputProgram[count + 1]);
-                    parameter2 = Integer.parseInt(inputProgram[count + 2]);
-                    parameter3 = Integer.parseInt(inputProgram[count + 3]);
-                    if (positionMode1) parameter1 = Integer.parseInt(inputProgram[parameter1]);
-                    if (positionMode2) parameter2 = Integer.parseInt(inputProgram[parameter2]);
-
-                    inputProgram[parameter3] = (oppCode == 1) ? String.valueOf(parameter1 + parameter2) : String.valueOf(parameter1 * parameter2);
-                    count += 4;
-                    break;
-                }
-                case 3: {
-                    parameter1 = Integer.parseInt(inputProgram[count + 1]);
-                    inputProgram[parameter1] = (phase) ? String.valueOf(phaseSetting) : String.valueOf(inputSetting);
-                    phase = false;
-                    count += 2;
-                    break;
-                }
-                case 4: {
-                    parameter1 = (positionMode1) ?
-                            Integer.parseInt(inputProgram[Integer.parseInt(inputProgram[count + 1])]) :
-                            Integer.parseInt(inputProgram[count + 1]);
-                    result = parameter1;
-                    count += 2;
-                    break;
-                }
-                case 5:
-                case 6: {
-                    parameter1 = Integer.parseInt(inputProgram[count + 1]);
-                    parameter2 = Integer.parseInt(inputProgram[count + 2]);
-
-                    if (positionMode1) parameter1 = Integer.parseInt(inputProgram[parameter1]);
-                    if (positionMode2) parameter2 = Integer.parseInt(inputProgram[parameter2]);
-
-                    count = (((oppCode == 5) && (parameter1 != 0)) || ((oppCode == 6 ) && (parameter1 == 0))) ? parameter2 : count + 3;
-                    break;
-                }
-                case 7:
-                case 8: {
-                    parameter1 =  Integer.parseInt(inputProgram[count + 1]);
-                    parameter2 =  Integer.parseInt(inputProgram[count + 2]);
-                    parameter3 =  Integer.parseInt(inputProgram[count + 3]);
-
-                    if (positionMode1) parameter1 = Integer.parseInt(inputProgram[parameter1]);
-                    if (positionMode2) parameter2 = Integer.parseInt(inputProgram[parameter2]);
-
-                    boolean conditionMet = (oppCode == 7) ? (parameter1 < parameter2) : (parameter1 == parameter2);
-                    inputProgram[parameter3] = (conditionMet) ? String.valueOf(1) : String.valueOf(0);
-
-                    count += 4;
-                    break;
-                }
-            }
-        }
-        return result;
+    private boolean isSequence01234(String s) {
+        return  (StringHelper.countChars(s,'0') == 1) &&
+                (StringHelper.countChars(s,'1') == 1) &&
+                (StringHelper.countChars(s,'2') == 1) &&
+                (StringHelper.countChars(s,'3') == 1) &&
+                (StringHelper.countChars(s,'4') == 1) ;
     }
 
-    private List<String> generateInputSequencesPart1 (){
-        List<String> result = new ArrayList<>();
-        for (int a=0; a <= 4; a++) {
-            for (int b=0; b <= 4; b++) {
-                for (int c=0; c <= 4; c++) {
-                    for (int d=0; d <= 4; d++) {
-                        for (int e=0; e <= 4; e++) {
-                            String s = a + String.valueOf(b) + c + d + e;
-                            if ((StringHelper.countChars(s,'0') == 1) &&
-                                (StringHelper.countChars(s,'1') == 1) &&
-                                (StringHelper.countChars(s,'2') == 1) &&
-                                (StringHelper.countChars(s,'3') == 1) &&
-                                (StringHelper.countChars(s,'4') == 1)) result.add(s);
-                        }
-                    }
-                }
-            }
-        }
-        return result;
+    private List<String> generateInputSequences01234 (){
+        return IntStream.rangeClosed(0, 44444).boxed().map(StringHelper::intToString5).
+                filter(this::isSequence01234).collect(Collectors.toList());
     }
 
-    private List<String> generateInputSequencesPart2 (){
-        List<String> result = new ArrayList<>();
-        for (int a=5; a <= 9; a++) {
-            for (int b=5; b <= 9; b++) {
-                for (int c=5; c <= 9; c++) {
-                    for (int d=5; d <= 9; d++) {
-                        for (int e=5; e <= 9; e++) {
-                            String s =  String.valueOf(a) + b + c + d + e;
-                            if ((StringHelper.countChars(s,'5') == 1) &&
-                                    (StringHelper.countChars(s,'6') == 1) &&
-                                    (StringHelper.countChars(s,'7') == 1) &&
-                                    (StringHelper.countChars(s,'8') == 1) &&
-                                    (StringHelper.countChars(s,'9') == 1)) result.add(s);
-                        }
-                    }
-                }
-            }
-        }
-        return result;
+    private boolean isSequence56789(String s) {
+        return  (StringHelper.countChars(s,'5') == 1) &&
+                (StringHelper.countChars(s,'6') == 1) &&
+                (StringHelper.countChars(s,'7') == 1) &&
+                (StringHelper.countChars(s,'8') == 1) &&
+                (StringHelper.countChars(s,'9') == 1) ;
     }
 
-
-
+    private List<String> generateInputSequences56789 (){
+        return IntStream.rangeClosed(55555, 99999).boxed().map(StringHelper::intToString5).
+                filter(this::isSequence56789).collect(Collectors.toList());
+    }
 
     @Override
     public String part1(List<String> input) {
         int maxValue = 0;
 
-        for (String inputSequence : generateInputSequencesPart1()) {
+        for (String inputSequence : generateInputSequences01234()) {
+            List<Amplifier> amplifiers = new ArrayList<>();
+            for (int x=0; x < 5; x++) {
+                Amplifier amplifier = new Amplifier(input.toArray(new String[0]), x);
+                amplifiers.add(amplifier);
+            }
+
             Integer output = 0;
-            for (int i = 0; i < inputSequence.length(); i++) {
-                output = runProgram(input.toArray(new String[0]), Integer.parseInt(String.valueOf(inputSequence.charAt(i))), output);
+            for (int y=0; y<5; y++) {
+                amplifiers.get(y).setPhaseSetting(Integer.parseInt(String.valueOf(inputSequence.charAt(y))));
+                output = amplifiers.get(y).runAmplifier(output);
             }
             if (output > maxValue) maxValue = output;
         }
-
         return input.isEmpty() ? "" : String.valueOf(maxValue);
     }
 
@@ -306,8 +190,7 @@ public class Day07 implements Day {
     public String part2(List<String> input) {
         int maxValue = 0;
 
-
-        for (String inputSequence : generateInputSequencesPart2()) {
+        for (String inputSequence : generateInputSequences56789()) {
             List<Amplifier> amplifiers = new ArrayList<>();
             for (int x=0; x < 5; x++) {
                 Amplifier amplifier = new Amplifier(input.toArray(new String[0]), x);
@@ -317,9 +200,7 @@ public class Day07 implements Day {
             boolean stop = (amplifiers.get(0)).isHalted();
             Integer output = 0;
             while (!stop) {
-
                 stop = true;
-
                 for (int y=0; y<5; y++)
                 if (!(amplifiers.get(y).isHalted())) {
                     amplifiers.get(y).setPhaseSetting(Integer.parseInt(String.valueOf(inputSequence.charAt(y))));
@@ -330,7 +211,6 @@ public class Day07 implements Day {
             }
             if (output > maxValue) maxValue = output;
         }
-
         return input.isEmpty() ? "" : String.valueOf(maxValue);
     }
 }
--- src/main/java/aoc/helper/StringHelper.java
+++ src/main/java/aoc/helper/StringHelper.java
@@ -10,17 +10,12 @@ public class StringHelper {
         return CharMatcher.is(testChar).countIn(testString);
     }
 
-    public static String intToString(int num, int digits) {
-        assert digits > 0 : "Invalid number of digits";
-
-        // create variable length array of zeros
+    public static String intToString5(int num) {
+        int digits = 5;
         char[] zeros = new char[digits];
         Arrays.fill(zeros, '0');
-        // format number as String
         DecimalFormat df = new DecimalFormat(String.valueOf(zeros));
-
         return df.format(num);
-
     }
 
 
