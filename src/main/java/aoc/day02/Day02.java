b97c5283015e3710928bfa91e78c65705d1840f1
oy29yv <marco.de.bie@ing.com>
2019-12-07 20:02:39 +0100

    Day02 refactored

--- src/main/java/aoc/day02/Day02.java
+++ src/main/java/aoc/day02/Day02.java
@@ -6,26 +6,21 @@ import java.util.List;
 
 public class Day02 implements Day {
 
-    private String[] runProgram(String[] inputProgram){
+    private Integer[] runProgram(Integer[] inputProgram){
         for (int i = 0; i < inputProgram.length; i += 4) {
-            if (Integer.parseInt(inputProgram[i]) == 99) break;
+            if (inputProgram[i] == 99) break;
 
-            if (Integer.parseInt(inputProgram[i]) == 1) {
-                inputProgram[Integer.parseInt(inputProgram[i + 3])] = String.valueOf(
-                        Integer.parseInt(inputProgram[Integer.parseInt(inputProgram[i + 1])]) +
-                                Integer.parseInt(inputProgram[Integer.parseInt(inputProgram[i + 2])]));
-            } else {
-                inputProgram[Integer.parseInt(inputProgram[i + 3])] = String.valueOf(
-                        Integer.parseInt(inputProgram[Integer.parseInt(inputProgram[i + 1])]) *
-                                Integer.parseInt(inputProgram[Integer.parseInt(inputProgram[i + 2])]));
-            }
+            inputProgram[inputProgram[i + 3]] = (inputProgram[i] == 1) ?
+                inputProgram[inputProgram[i + 1]] + inputProgram[inputProgram[i + 2]] :
+                inputProgram[inputProgram[i + 1]] * inputProgram[inputProgram[i + 2]];
+
         }
         return inputProgram;
     }
 
     @Override
     public String part1(List<String> input) {
-        return input.isEmpty() ? "" : runProgram(input.toArray(new String[0]))[0];
+        return input.isEmpty() ? "" : String.valueOf(runProgram(input.stream().map(Integer::parseInt).toArray(Integer[]::new))[0]);
     }
 
     @Override
@@ -36,10 +31,10 @@ public class Day02 implements Day {
 
         for (a = 0; a <= 99; a++) {
             for (b = 0; b <= 99; b++) {
-                String[] programArray = input.toArray(new String[0]);
-                programArray[1] = String.valueOf(a);
-                programArray[2] = String.valueOf(b);
-                found = (Integer.parseInt(runProgram(programArray)[0]) == 19690720);
+                Integer[] programArray = input.stream().map(Integer::parseInt).toArray(Integer[]::new);
+                programArray[1] = a;
+                programArray[2] = b;
+                found = (runProgram(programArray)[0] == 19690720);
                 if (found) break;
             }
             if (found) break;
