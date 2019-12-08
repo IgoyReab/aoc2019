bea8a73c380a7b0721a67ebb80cd1ea66dd2854e
oy29yv <marco.de.bie@ing.com>
2019-12-08 21:59:36 +0100

    Day08 refactored

--- src/main/java/aoc/day08/Day08.java
+++ src/main/java/aoc/day08/Day08.java
@@ -20,14 +20,65 @@ public class Day08 implements Day {
     class Image {
         private List<Layer> layers;
         private Integer[][] finalImage;
+        private int countWidth = 0;
+        private int countTall = 0;
+        private int count = 0;
+        private int layerCount = 0;
+        private boolean newLayer = true;
 
-        public Image() {
+        public Image(List<String> input) {
             layers = new ArrayList<>();
             this.finalImage = new Integer[tall][width];
             loop(finalImage, (y , x) -> finalImage[y][x] = 2);
+
+            List<Integer> inputIntegers = StringHelper.convertStringToIntegerList(input.get(0));
+            inputIntegers.forEach(this::mapIntegers);
+
+            printImage();
         }
 
-        public void printImage() {
+        private void mapIntegers(int i){
+            if (newLayer) {
+                addLayer(new Layer());
+                newLayer = false;
+            }
+
+            getLayers().get(layerCount).rows[countTall][countWidth] = i;
+            if (finalImage[countTall][countWidth] == 2) {
+                finalImage[countTall][countWidth] = i;
+            }
+
+            int numberOfZeroes = (i == 0) ?
+                    getLayers().get(layerCount).getNumberOfZeroes() + 1 :
+                    getLayers().get(layerCount).getNumberOfZeroes();
+            int numberOfOnes = (i == 1) ?
+                    getLayers().get(layerCount).getNumberOfOnes() + 1 :
+                    getLayers().get(layerCount).getNumberOfOnes();
+            int numberOfTwos = (i == 2) ?
+                    getLayers().get(layerCount).getNumberOfTwos() + 1 :
+                    getLayers().get(layerCount).getNumberOfTwos();
+
+            getLayers().get(layerCount).setNumberOfZeroes(numberOfZeroes);
+            getLayers().get(layerCount).setNumberOfOnes(numberOfOnes);
+            getLayers().get(layerCount).setNumberOfTwos(numberOfTwos);
+
+            if (countWidth == (width - 1)) {
+                countWidth = 0;
+                if (countTall == (tall - 1)) {
+                    countTall = 0;
+                    newLayer = true;
+                    layerCount++;
+                } else {
+                    countTall++;
+                }
+            } else {
+                countWidth++;
+            }
+
+            count++;
+        }
+
+        private  void printImage() {
             System.out.println("Image : \n");
             loop(finalImage, (y, x) -> {
                 if (x==0) System.out.println();
@@ -36,61 +87,11 @@ public class Day08 implements Day {
             System.out.println("\n\n");
         }
 
-        public void makeImage(List<String> input){
-            int countWidth = 0;
-            int countTall = 0;
-            int count = 0;
-            int layerCount = 0;
-            boolean newLayer = true;
-            List<Integer> inputIntegers = StringHelper.convertStringToIntegerList(input.get(0));
-
-            while (count < inputIntegers.size()) {
-                if (newLayer) {
-                    addLayer(new Layer());
-                    newLayer = false;
-                }
-
-                getLayers().get(layerCount).rows[countTall][countWidth] = inputIntegers.get(count);
-                if (finalImage[countTall][countWidth] == 2) {
-                    finalImage[countTall][countWidth] = inputIntegers.get(count);
-                }
-
-                int numberOfZeroes = (inputIntegers.get(count) == 0) ?
-                        getLayers().get(layerCount).getNumberOfZeroes() + 1 :
-                        getLayers().get(layerCount).getNumberOfZeroes();
-                int numberOfOnes = (inputIntegers.get(count) == 1) ?
-                        getLayers().get(layerCount).getNumberOfOnes() + 1 :
-                        getLayers().get(layerCount).getNumberOfOnes();
-                int numberOfTwos = (inputIntegers.get(count) == 2) ?
-                        getLayers().get(layerCount).getNumberOfTwos() + 1 :
-                        getLayers().get(layerCount).getNumberOfTwos();
-
-                getLayers().get(layerCount).setNumberOfZeroes(numberOfZeroes);
-                getLayers().get(layerCount).setNumberOfOnes(numberOfOnes);
-                getLayers().get(layerCount).setNumberOfTwos(numberOfTwos);
-
-                if (countWidth == (width - 1)) {
-                    countWidth = 0;
-                    if (countTall == (tall - 1)) {
-                        countTall = 0;
-                        newLayer = true;
-                        layerCount++;
-                    } else {
-                        countTall++;
-                    }
-                } else {
-                    countWidth++;
-                }
-
-                count++;
-            }
-        }
-
-        public void addLayer(Layer inputLayer) {
+        private void addLayer(Layer inputLayer) {
             layers.add(inputLayer);
         }
 
-        public Layer getLayerWithMinimalZeroes() {
+        private Layer getLayerWithMinimalZeroes() {
             Integer zeroes = MAX_VALUE;
             Layer result = null;
 
@@ -122,18 +123,13 @@ public class Day08 implements Day {
 
     @Override
     public String part1(List<String> input) {
-        Image image = new Image();
-        image.makeImage(input);
+        Image image = new Image(input);
         return input.isEmpty() ? "" : String.valueOf(image.getLayerWithMinimalZeroes().getNumberOfOnes() * image.getLayerWithMinimalZeroes().getNumberOfTwos());
     }
 
     @Override
     public String part2(List<String> input) {
-
-        Image image = new Image();
-        image.makeImage(input);
-        image.printImage();
-
+        Image image = new Image(input);
         return input.isEmpty() ? "" : Arrays.deepToString(image.finalImage);
     }
 }
