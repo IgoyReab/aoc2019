4446c0b463327d7772756636f77ebd477862d2f1
oy29yv <marco.de.bie@ing.com>
2019-12-09 22:56:49 +0100

    WIP Day09 improving

--- src/main/java/aoc/day09/Day09.java
+++ src/main/java/aoc/day09/Day09.java
@@ -29,168 +29,163 @@ public class Day09 implements Day {
             this.result = new ArrayList<>();
             this.relativeBias = 0;
             this.inputProgram = inputProgram;
-            for (int x = inputProgram.size(); x < 100000; x++) inputProgram.add((long) 0);
+            // for (int x = inputProgram.size(); x < 100000; x++) inputProgram.add((long) 0);
+        }
+
+        private long getParameter(int index) {
+            try {
+                return inputProgram.get(index);
+            } catch ( IndexOutOfBoundsException e ) {
+                return 0;
+            }
+        }
+
+        private long getParameter(long index) {
+            try {
+                return inputProgram.get((int) index);
+            } catch ( IndexOutOfBoundsException e ) {
+                return 0;
+            }
+        }
+
+        private int getOppcode() {
+            return (int) ((((getParameter(count) / 10) % 10) * 10) + (getParameter(count) % 10));
+        }
+
+        private long checkMode(long parameter, boolean positionMode , boolean relativeMode) {
+            long result = parameter;
+            if (positionMode) parameter1 = getParameter(parameter1);
+            if (relativeMode) {
+                long newIndex = parameter + relativeBias;
+                if (newIndex >= 0) {
+                    result = getParameter(newIndex);
+                }
+            }
+            return result;
+        }
+
+        private long checkMode(long parameter, boolean relativeMode) {
+            long result = parameter;
+
+            if (relativeMode) {
+                long newIndex = parameter + relativeBias;
+                if (newIndex >= 0) {
+                    result = getParameter(newIndex);
+                }
+            }
+            return result;
+        }
+
+        private long checkIndex(long parameter, boolean relativeMode) {
+            long result = parameter;
+
+            if (relativeMode) {
+                long newIndex = parameter + relativeBias;
+                if (newIndex >= 0) {
+                    result = newIndex;
+                }
+            }
+            return result;
         }
 
         public List<Long> runIntegerComputer() {
             while ( oppCode != 99 ) {
-                oppCode = (int) ((((inputProgram.get(count) / 10) % 10) * 10) + (inputProgram.get(count) % 10));
-                boolean positionMode1 = (0 == ( inputProgram.get(count) / 100) % 10);
-                boolean positionMode2 = (0 == ( inputProgram.get(count) / 1000) % 10);
-                boolean positionMode3 = (0 == ( inputProgram.get(count) / 10000) % 100);
-                boolean immediateMode1 = (1 == ( inputProgram.get(count) / 100) % 10);
-                boolean immediateMode2 = (1 == ( inputProgram.get(count) / 1000) % 10);
-                boolean immediateMode3 = (1 == ( inputProgram.get(count) / 10000) % 100);
-                boolean relativeMode1 = (2 == ( inputProgram.get(count) / 100) % 10);
-                boolean relativeMode2 = (2 == ( inputProgram.get(count) / 1000) % 10);
-                boolean relativeMode3 = (2 == ( inputProgram.get(count) / 10000) % 10);
+                oppCode = getOppcode();
 
-                // System.out.println("Program : " + inputProgram.get(count) + " " + inputProgram.get(count + 1) + " " + inputProgram.get(count + 2) + " " + inputProgram.get(count + 3) );
+                boolean positionMode1 = (0 == ( getParameter(count) / 100) % 10);
+                boolean positionMode2 = (0 == ( getParameter(count) / 1000) % 10);
+
+                boolean relativeMode1 = (2 == ( getParameter(count) / 100) % 10);
+                boolean relativeMode2 = (2 == ( getParameter(count) / 1000) % 10);
+                boolean relativeMode3 = (2 == ( getParameter(count) / 10000) % 10);
 
                 switch (oppCode) {
-                    case 99:
-                        break;
                     case 1:
                     case 2: {
-                        parameter1 = inputProgram.get(count + 1);
-                        parameter2 = inputProgram.get(count + 2);
-                        parameter3 = inputProgram.get(count + 3);
-
-                        if (positionMode1) parameter1 = inputProgram.get((int) parameter1);
-                        if (positionMode2) parameter2 = inputProgram.get((int) parameter2);
-                        // if (positionMode3) parameter3 = inputProgram.get((int) parameter3);
-
-                        if (relativeMode1) {
-                            int newIndex = (int)(parameter1 + relativeBias);
-                            if (newIndex >= 0) {
-                                parameter1 = inputProgram.get(newIndex);
-                            }
-                        }
-
-                        if (relativeMode2) {
-                            int newIndex = (int)(parameter2 + relativeBias);
-                            if (newIndex >= 0) {
-                                parameter2 = inputProgram.get(newIndex);
-                            }
-                        }
-
-                        if (relativeMode3) {
-                            int newIndex = (int)(parameter3 + relativeBias);
-                            if (newIndex >= 0) {
-                                parameter3 = newIndex;
-                            }
-                        }
-
-
+                        parameter1 = checkMode(getParameter(count + 1), positionMode1, relativeMode1);
+                        parameter2 = checkMode(getParameter(count + 2), positionMode2, relativeMode2);
+                        parameter3 = checkIndex(getParameter(count + 3), relativeMode3);
                         if (oppCode == 1) {
-                            inputProgram.set((int) parameter3, parameter1 + parameter2);
+                            try {
+                                inputProgram.set((int) parameter3, parameter1 + parameter2);
+                            } catch ( IndexOutOfBoundsException e ) {
+                                for (int x = inputProgram.size(); x < parameter3; x++) {
+                                    inputProgram.add(x, (long) 0);
+                                }
+                                inputProgram.add((int) parameter3, parameter1 + parameter2);
+                            }
                         } else {
-                            inputProgram.set((int) parameter3, parameter1 * parameter2);
+                            try {
+                                inputProgram.set((int) parameter3, parameter1 * parameter2);
+                            } catch ( IndexOutOfBoundsException e ) {
+                                for (int x = inputProgram.size(); x < parameter3; x++) inputProgram.add(x, (long) 0);
+                                inputProgram.add((int) parameter3, parameter1 * parameter2);
+                            }
                         }
                         count += 4;
                         break;
                     }
+
                     case 3: {
-                        parameter1 = inputProgram.get(count + 1);
-                        if (positionMode1) parameter1 = inputProgram.get((int) parameter1);
-                        if (relativeMode1) {
-                            int newIndex = (int)(parameter1 + relativeBias);
-                            if (newIndex >= 0) parameter1 = newIndex;
+                        parameter1 = checkMode(getParameter(count + 1), positionMode1, relativeMode1);
+                        try {
+                            inputProgram.set((int) parameter1, initialInput);
+                        } catch (IndexOutOfBoundsException e ) {
+                            for (int x = inputProgram.size(); x < parameter1; x++) inputProgram.add(x, (long) 0);
+                            inputProgram.add((int) parameter1, initialInput);
                         }
-                        inputProgram.set((int) parameter1, initialInput);
                         count += 2;
                         break;
                     }
+
                     case 4: {
-                        parameter1 = inputProgram.get(count + 1);
-                        if (positionMode1) {
-                            parameter1 = inputProgram.get((int) parameter1);
-                        }
-
-                        if (relativeMode1) {
-                            int newIndex = ((int)(parameter1 + relativeBias));
-                            if (newIndex >= 0) parameter1 = inputProgram.get(newIndex);
-                        }
-
-                        result.add(parameter1);
+                        result.add(checkMode(getParameter(count + 1), positionMode1, relativeMode1));
                         count += 2;
                         break;
                     }
+
                     case 5:
                     case 6: {
-                        parameter1 = inputProgram.get(count + 1);
-                        parameter2 = inputProgram.get(count + 2);
-
-                        if (positionMode1) parameter1 = inputProgram.get((int) parameter1);
-                        if (positionMode2) parameter2 = inputProgram.get((int) parameter2);
-
-                        if (relativeMode1) {
-                            int newIndex = ((int)(parameter1 + relativeBias));
-                            if (newIndex >= 0) parameter1 = inputProgram.get(newIndex);
-                        }
-
-                        if (relativeMode2) {
-                            int newIndex = ((int)(parameter2 + relativeBias));
-                            if (newIndex >= 0) parameter2 = inputProgram.get(newIndex);
-                        }
-
-                        if (relativeMode3) {
-                            int newIndex = ((int)(parameter3 + relativeBias));
-                            if (newIndex >= 0) parameter3 = inputProgram.get(newIndex);
-                        }
-
+                        parameter1 = checkMode(getParameter(count + 1), positionMode1, relativeMode1);
+                        parameter2 = checkMode(getParameter(count + 2), positionMode2, relativeMode1);
                         count += 3;
                         if ((oppCode == 5) && (parameter1 != 0)) count = (int) parameter2;
                         if ((oppCode == 6) && (parameter1 == 0)) count = (int) parameter2;
-
                         break;
                     }
+
                     case 7:
                     case 8: {
-                        parameter1 = inputProgram.get(count + 1);
-                        parameter2 = inputProgram.get(count + 2);
-                        parameter3 = inputProgram.get(count + 3);
-
-                        if (positionMode1) parameter1 = inputProgram.get((int) parameter1);
-                        if (positionMode2) parameter2 = inputProgram.get((int) parameter2);
-                        // if (positionMode3) parameter3 = inputProgram.get((int) parameter3);
-
-                        if (relativeMode1) {
-                            int newIndex = ((int)(parameter1 + relativeBias));
-                            if (newIndex >= 0) parameter1 = inputProgram.get(newIndex);
-                        }
-
-                        if (relativeMode2) {
-                            int newIndex = ((int)(parameter2 + relativeBias));
-                            if (newIndex >= 0) parameter2 = inputProgram.get(newIndex);
-                        }
-
-                        if (relativeMode3) {
-                            int newIndex = ((int)(parameter3 + relativeBias));
-                            if (newIndex >= 0) parameter3 = newIndex;
-                        }
-
+                        parameter1 = checkMode(getParameter(count + 1), positionMode1, relativeMode1);
+                        parameter2 = checkMode(getParameter(count + 2), positionMode2, relativeMode2);
+                        parameter3 = checkIndex(getParameter(count + 3), relativeMode3);
                         boolean conditionMet = (oppCode == 7) ? (parameter1 < parameter2) : (parameter1 == parameter2);
                         if (conditionMet) {
-                            inputProgram.set((int) parameter3, (long) 1);
+                            try {
+                                inputProgram.set((int) parameter3, (long) 1);
+                            } catch (IndexOutOfBoundsException e ) {
+                                for (int x = inputProgram.size(); x < parameter1; x++) inputProgram.add(x, (long) 0);
+                                inputProgram.add((int) parameter3, (long) 1);
+                            }
                         } else {
-                            inputProgram.set((int) parameter3, (long) 0);
+                            try {
+                                inputProgram.set((int) parameter3, (long) 0);
+                            } catch (IndexOutOfBoundsException e ) {
+                                for (int x = inputProgram.size(); x < parameter1; x++) inputProgram.add(x, (long) 0);
+                                inputProgram.add((int) parameter3, (long) 0);
+                            }
                         }
-
                         count += 4;
                         break;
                     }
+
                     case 9: {
-                        parameter1 = inputProgram.get(count + 1);
-
-                        if (positionMode1) parameter1 = inputProgram.get((int) parameter1);
-
-                        if (relativeMode1) {
-                            int newIndex = (int) (relativeBias + parameter1);
-                            if (newIndex >= 0) parameter1 = inputProgram.get(newIndex);
-                        }
+                        parameter1 = checkMode(getParameter(count + 1), positionMode1, relativeMode1);
                         relativeBias += parameter1;
                         count+=2;
+                    }
+                    case 99: {
+                        break;
                     }
                 }
             }
@@ -215,9 +210,9 @@ public class Day09 implements Day {
     }
 
     @Override
-    public String part2(List<String> input) {List<Long> longInput = input.stream().
+    public String part2(List<String> input) {
+        List<Long> longInput = input.stream().
             map(Long::parseLong).collect(Collectors.toList());
-
 
         IntegerComputer ic = new IntegerComputer(longInput, 2);
         initialInput = 2;
