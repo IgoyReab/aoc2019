c200a34a0d19329e553f7f7bc79bc9f6db7a0300
oy29yv <marco.de.bie@ing.com>
2019-12-16 17:52:49 +0100

    Day16 part1 finished

--- src/main/java/aoc/day16/Day16.java
+++ src/main/java/aoc/day16/Day16.java
@@ -2,18 +2,117 @@ package aoc.day16;
 
 import aoc.Day;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.lang.Math.abs;
 
 public class Day16 implements Day {
 
 
+    private List<Integer> expandList (List<Integer> input, int factor) {
+        List<Integer> result = new ArrayList<>();
+
+        for (int x=0; x< input.size(); x++) {
+            for (int y=0; y<factor; y++) {
+                result.add(input.get(x));
+            }
+        }
+        return result;
+    }
+
+    private List<Integer> parsePhase (List<Integer> input, List<Integer> parse) {
+        List<Integer> result = new ArrayList<>();
+
+        for (int a=0; a<input.size(); a++) {
+            List<Integer> thisParse = expandList(parse, a+1);
+            int newDigit= 0;
+            int y = 0;
+            for (int x=0; x<input.size(); x++) {
+                y++;
+                if (y>=thisParse.size()) y=0;
+                newDigit = newDigit + (input.get(x) * thisParse.get(y));
+            }
+            result.add(abs(newDigit)%10);
+        }
+        return result;
+    }
+
+
     @Override
     public String part1(List<String> input) {
-        return input.isEmpty() ? "" : "";
+
+        // Create an empty List of character
+        List<Character> chars = input.get(0)
+                .chars()
+                .mapToObj(e -> (char)e)
+                .collect(Collectors.toList());
+
+        List<Integer> inputList = chars.stream().map(e -> Integer.parseInt(String.valueOf(e))).collect(Collectors.toList());
+
+        System.out.println(inputList);
+
+        List<Integer> parseList = new ArrayList<>();
+        parseList.add(0);
+        parseList.add(1);
+        parseList.add(0);
+        parseList.add(-1);
+
+        System.out.println(parseList);
+
+        List<Integer> result = parsePhase(inputList, parseList);
+        for (int x=1; x<100; x++) {
+            result = parsePhase(result, parseList);
+        }
+
+        System.out.println(result);
+        return input.isEmpty() ? "" : String.valueOf(result);
     }
 
     @Override
     public String part2(List<String> input) {
-        return input.isEmpty() ? "" : "";
+        List<Character> chars = input.get(0)
+                .chars()
+                .mapToObj(e -> (char)e)
+                .collect(Collectors.toList());
+
+        List<Integer> initialList = chars.stream().map(e -> Integer.parseInt(String.valueOf(e))).collect(Collectors.toList());
+
+        String offSet = "";
+        for (int count=0; count<7; count++) {
+            offSet = offSet + initialList.get(count);
+        }
+
+        int offSetNum = Integer.parseInt(offSet);
+
+        List<Integer> inputList = new ArrayList<>();
+
+        for (int count=0; count<10000; count++) {
+            inputList.addAll(initialList);
+        }
+
+        List<Integer> parseList = new ArrayList<>();
+        parseList.add(0);
+        parseList.add(1);
+        parseList.add(0);
+        parseList.add(-1);
+
+        System.out.println(parseList);
+
+        List<Integer> result = parsePhase(inputList, parseList);
+        for (int x=1; x<100; x++) {
+            result = parsePhase(result, parseList);
+        }
+
+        String resultString = "";
+        for (int count=offSetNum; count<offSetNum+8; count++) {
+            resultString = resultString + result.get(count);
+        }
+
+        System.out.println(resultString);
+
+
+        return input.isEmpty() ? "" : resultString;
     }
 }
--- src/test/java/aoc/day16/Day16Test.java
+++ src/test/java/aoc/day16/Day16Test.java
@@ -1,6 +1,5 @@
 package aoc.day16;
 
-import aoc.day25.Day25;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -11,25 +10,83 @@ import static org.junit.Assert.assertEquals;
 public class Day16Test {
 
     @Test
-    public void testPart1(){
+    public void testPart1_1(){
         List<String> input;
         String result;
 
-        input = Arrays.asList("1","2");
+        input = Arrays.asList("12345678");
 
         result = new Day16().part1(input);
-        assertEquals("", result);
+        assertEquals("[2, 3, 8, 4, 5, 6, 7, 8]", result);
     }
 
     @Test
-    public void testPart2(){
+    public void testPart1_2(){
         List<String> input;
         String result;
 
-        input = Arrays.asList("1","2");
+        input = Arrays.asList("80871224585914546619083218645595");
+
+        result = new Day16().part1(input);
+        assertEquals("[2, 4, 1, 7, 6, 1, 7, 6, 4, 8, 0, 9, 1, 9, 0, 4, 6, 1, 1, 4, 0, 3, 8, 7, 6, 3, 1, 9, 5, 5, 9, 5]", result);
+    }
+
+    @Test
+    public void testPart1_3(){
+        List<String> input;
+        String result;
+
+        input = Arrays.asList("19617804207202209144916044189917");
+
+        result = new Day16().part1(input);
+        assertEquals("[7, 3, 7, 4, 5, 4, 1, 8, 5, 5, 7, 2, 5, 7, 2, 5, 9, 1, 4, 9, 4, 6, 6, 5, 9, 9, 6, 3, 9, 9, 1, 7]", result);
+    }
+
+    @Test
+    public void testPart1_4(){
+        List<String> input;
+        String result;
+
+        input = Arrays.asList("69317163492948606335995924319873");
+
+        result = new Day16().part1(input);
+        assertEquals("[5, 2, 4, 3, 2, 1, 3, 3, 2, 9, 2, 9, 9, 8, 6, 0, 6, 8, 8, 0, 4, 9, 5, 9, 7, 4, 8, 6, 9, 8, 7, 3]", result);
+    }
+
+
+    @Test
+    public void testPart2_1(){
+        List<String> input;
+        String result;
+
+        input = Arrays.asList("03036732577212944063491565474664");
 
         result = new Day16().part2(input);
-        assertEquals("", result);
+        assertEquals("84462026", result);
+
+    }
+
+    @Test
+    public void testPart2_2(){
+        List<String> input;
+        String result;
+
+        input = Arrays.asList("02935109699940807407585447034323");
+
+        result = new Day16().part2(input);
+        assertEquals("78725270", result);
+
+    }
+
+    @Test
+    public void testPart2_3(){
+        List<String> input;
+        String result;
+
+        input = Arrays.asList("03081770884921959731165446850517");
+
+        result = new Day16().part2(input);
+        assertEquals("53553731", result);
 
     }
 }
